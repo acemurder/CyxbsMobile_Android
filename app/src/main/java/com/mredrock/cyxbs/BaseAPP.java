9c913c399e170dae6d44828010be0c55bd698e50
zxzhu <354328995@qq.com>
2018-04-13 21:27:35 +0800

    nothing

--- app/src/main/java/com/mredrock/cyxbs/BaseAPP.java
+++ app/src/main/java/com/mredrock/cyxbs/BaseAPP.java
@@ -23,7 +23,6 @@ import com.mredrock.cyxbs.util.Utils;
 import com.orhanobut.logger.Logger;
 import com.taobao.sophix.PatchStatus;
 import com.taobao.sophix.SophixManager;
-import com.taobao.sophix.listener.PatchLoadStatusListener;
 import com.tencent.bugly.crashreport.CrashReport;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.message.IUmengRegisterCallback;
@@ -164,21 +163,17 @@ public class BaseAPP extends MultiDexApplication {
                 .setAppVersion(String.valueOf(Utils.getAppVersionCode(getContext())) + ".0")
                 .setAesKey(null)
                 .setEnableDebug(true)
-                .setPatchLoadStatusStub(new PatchLoadStatusListener() {
-                    @Override
-                    public void onLoad(final int mode, final int code, final String info, final int handlePatchVersion) {
-                        // 补丁加载回调通知
-                        if (code == PatchStatus.CODE_LOAD_SUCCESS) {
-                            // 表明补丁加载成功
-                        } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
-                            // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
-                            // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
-                        } else {
-                            // 其它错误信息, 查看PatchStatus类说明
-                        }
+                .setPatchLoadStatusStub((mode, code, info, handlePatchVersion) -> {
+                    // 补丁加载回调通知
+                    if (code == PatchStatus.CODE_LOAD_SUCCESS) {
+                        // 表明补丁加载成功
+                    } else if (code == PatchStatus.CODE_LOAD_RELAUNCH) {
+                        // 表明新补丁生效需要重启. 开发者可提示用户或者强制重启;
+                        // 建议: 用户可以监听进入后台事件, 然后调用killProcessSafely自杀，以此加快应用补丁，详见1.3.2.3
+                    } else {
+                        // 其它错误信息, 查看PatchStatus类说明
                     }
                 }).initialize();
-        // queryAndLoadNewPatch不可放在attachBaseContext 中，否则无网络权限，建议放在后面任意时刻，如onCreate中
         SophixManager.getInstance().queryAndLoadNewPatch();
     }
 
